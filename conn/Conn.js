ba4df1dbcc1ebf78c73bd2de94da4d1bfd6f607d
MarianaMSA <maiasalvador@gmail.com>
2021-10-18 23:01:10 -0300

    up start node

--- conn/Conn.js
+++ conn/Conn.js
@@ -11,3 +11,19 @@ const Conn = () => {
 };
 
 module.exports = Conn;
+
+
+//////const mongoose = require('mongoose');
+
+// const Conn = (url, user, pass, data) => {
+//    mongoose.connect(`${url}/${data}`, {
+//        user: user,
+//        pass: pass,
+//        useNewUrlParser: true,
+//        useUnifiedTopology: true,
+//    })
+//    .then(() => console.log('MongoDB connected'))
+//    .catch((err) => console.error('Error at mongo', err))
+// }
+
+// module.exports = Conn;
